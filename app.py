224fd5afc26bb626e437eae9421d9c461d2c2ebc
Edward Prokopik <ejprok@gmail.com>
2018-04-07 18:41:13 +0000

    updated the index and requirements

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask, render_template
+import tensorflow as tf
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+	
+	hello = tf.constant('Hello, TensorFlow!')
+	
+	# Start tf session
+	sess = tf.Session()
+	
+	# Run the op
+	print(sess.run(hello))
+	
 	return render_template("index.html")
+	
+	
 	
 if __name__ == "__main__":
 	app.debug = True
